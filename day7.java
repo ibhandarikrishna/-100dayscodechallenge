16c25fb4c192c75c3dc6353d977f6883b805806e
Krishna Bhandari <115021507+ibhandarikrishna@users.noreply.github.com>
2022-10-10 22:50:21 +0530

    Create day7.java

---
+++ day7.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    Scanner x = new Scanner(System.in);
+    int line = 0;
+    
+    while(x.hasNextLine()){
+        line++;
+        System.out.println(line+" "+x.nextLine());
+        
+    }
+
+    }
+    }
