c0660b9709f564b1b109082e0b046882d98311cb
Krishna Bhandari <115021507+ibhandarikrishna@users.noreply.github.com>
2022-10-11 22:22:51 +0530

    Create day8.java

---
+++ day8.java
@@ -0,0 +1,24 @@
+public class Solution {
+
+static int B,H;
+static boolean flag = true;
+static{ //ONLY STATIC BLOCKS CAN ACCESS AND MODIFY STATIC VARIABLES.
+    Scanner sc = new Scanner(System.in);
+    B = sc.nextInt();
+    H = sc.nextInt();
+    if(B<=0 || H<=0)
+    {
+        flag = false;
+        System.out.println("java.lang.Exception: Breadth and height must be positive");
+    }
+ 
+
+}
+public static void main(String[] args){
+        if(flag){
+            int area=B*H;
+            System.out.print(area);
+        }
+        
+}
+}
